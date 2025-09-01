3ae99898164a5642409cf63105e770a5ace846b5
Lucas Baggio <lucas.baggio.silva@gmail.com>
2025-09-01 20:02:17 -0300

    Feat - First commit

---
+++ app/styles/mainStyle.ts
@@ -0,0 +1,35 @@
+import { StyleSheet } from "react-native";
+
+export const baseStyles = StyleSheet.create({
+    primaryBg: {
+        backgroundColor: "#0D0D0F"
+    },
+
+    secondaryBg: {
+        backgroundColor: "#1A1C20"
+    },
+
+    primaryColor: {
+        backgroundColor: "#00BFFF"
+    },
+
+    secondaryColor: {
+        backgroundColor: "#7A5CFF"
+    },
+
+    energy: {
+        backgroundColor: "#FF3C7E"
+    },
+
+    primaryText: {
+        color: "#E6E6E6"
+    },
+
+    secondaryText: {
+        color: "#A0A0A0"
+    },
+
+    success: {
+        color: "#00FF95"
+    }
+});
